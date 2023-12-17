1bb9f8adc08a6a528aab1d7c356ff400713fcd89
Wilmer Arambula <terabytesoftw@gmail.com>
2023-12-17 12:25:04 -0300

    Add camelCaseToSnakeCase and snakeCaseToCamelCase methods.

--- src/WordFormatter.php
+++ src/WordFormatter.php
@@ -7,7 +7,9 @@ namespace PHPForge\Helpers;
 use function explode;
 use function implode;
 use function preg_match;
+use function preg_replace;
 use function preg_split;
+use function str_contains;
 use function strtolower;
 use function ucfirst;
 
@@ -21,17 +23,18 @@ final class WordFormatter
      * Converts a string to words with capitalized first letters.
      *
      * This function takes a string as input and converts it into words with the first letter of each word capitalized.
-     * The input string can be in different formats, such as camelCase or snake_case, and the function will handle them properly.
-     * If the input string is in all uppercase, it will be treated as a single word and capitalized accordingly.
+     * The input string can be in different formats, such as camelCase or snake_case, and the function will handle them
+     * properly.
+     * If the input string is in uppercase, it will be treated as a single word and capitalized accordingly.
      *
-     * @param string $string The input string to be converted.
+     * @param string $value The input string to be converted.
      *
      * @return string The string with words having capitalized first letters separated by spaces.
      */
-    public static function capitalizeToWords(string $string): string
+    public static function capitalizeToWords(string $value): string
     {
-        if (preg_match('/^[A-Z][^_]*(_[A-Z][^_]*)*/', $string)) {
-            $strings = explode('_', $string);
+        if (preg_match('/^[A-Z][^_]*(_[A-Z][^_]*)*/', $value)) {
+            $strings = explode('_', $value);
             $word = '';
 
             foreach ($strings as $index => $string) {
@@ -45,12 +48,52 @@ final class WordFormatter
         }
 
         $capitalizedWords = [];
-        $words = preg_split('/(?=[A-Z])|_/', $string);
+        $words = preg_split('/(?=[A-Z])|_/', $value);
 
         foreach ($words as $word) {
             $capitalizedWords[] = ucfirst($word);
         }
 
         return implode(' ', $capitalizedWords);
+    }
+
+    /**
+     * Converts a camelCase formatted string to snake_case.
+     *
+     * @param string $value The camelCase formatted string to convert.
+     */
+    public static function camelCaseToSnakeCase(string $value): string
+    {
+        $snakeCase = preg_replace('/([A-Z])/', '_$1', $value);
+        $snakeCase = ltrim($snakeCase, '_'); // Eliminar guión bajo inicial si está presente
+        $snakeCase = strtolower($snakeCase);
+
+        return $snakeCase;
+    }
+
+    /**
+     * Convert a snake_case formatted string to camelCase.
+     *
+     * @param string $snakeCaseString The snake_case formatted string to convert.
+     * @return string The converted camelCase string.
+     */
+    public static function snakeCaseToCamelCase(string $snakeCaseString): string
+    {
+        if (str_contains($snakeCaseString, '_') === false) {
+            return $snakeCaseString;
+        }
+
+        $words = explode('_', $snakeCaseString);
+        $camelCase = '';
+
+        foreach ($words as $index => $word) {
+            if ($index === 0) {
+                $camelCase = $word;
+            } else {
+                $camelCase .= ucfirst($word);
+            }
+        }
+
+        return $camelCase;
     }
 }
